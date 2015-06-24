db60219a1446bb75dd98bfbb12ee6ec4eda6d6bb
Abdesslem <abdesslem@accessnow.org>
2015-06-24 14:57:34 +0100

    add structure for the pcaptotal API

---
+++ web/api.py
@@ -0,0 +1,49 @@
+from flask import jsonify, abort, make_response
+from flask.ext.httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
+from app import app
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+#@auth.login_required
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+#@auth.login_required
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@auth.get_password
+def get_password(username):
+    if username == 'ask3m':
+        return 'ask3m'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+
+
